dd6a4a2e5dfb7df416549ee5ebdb7982f0eb854d
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-07-28 14:39:08 +0200

    add docs

--- py_simple_package/src/py_simple/easy_validator.py
+++ py_simple_package/src/py_simple/easy_validator.py
@@ -2,9 +2,20 @@ import re
 
 
 def is_valid_email(email: str) -> bool:
+    """
+        Returns true if email is valid.
+
+        Arguments:
+            email (str) -- email adress to validate.
+
+        Example:
+            is_valid_email("mymail@gmail.com")
+            (True)
+            is_valid_email("email.com")
+            (False)
+        """
     pattern = r'[a-zA-Z_.%+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]+'
     if re.fullmatch(pattern, email):
         return True
     else:
         return False
-
