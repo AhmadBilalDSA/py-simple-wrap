657f7bca582498cc50d8c86f94523968f20f80d7
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-08-16 21:06:40 +0200

    preparing for release

--- py_simple_package/src/py_simple/__init__.py
+++ py_simple_package/src/py_simple/__init__.py
@@ -92,6 +92,7 @@ from .easy_math import (
     sum_of_digits, divisors,
 )
 
-from .easy_data_visualization import (
-    plot_data
-)
+# EXCLUDED FROM RELEASE UNTIL UNIT TESTS EXIST
+# from .easy_data_visualization import (
+#     plot_data
+# )
